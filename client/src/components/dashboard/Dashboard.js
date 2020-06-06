7ac41f904cbed8dda72abc788246b3f5b98c0bf9
vattcarter7 <vattcarter@gmail.com>
2020-06-06 22:13:06 +0700

    added profile-create form

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -27,7 +27,7 @@ const Dashboard = ({
         <Fragment>has</Fragment>
       ) : (
         <Fragment>
-          You have not yet setup a profile, please add some info
+          <p>You have not yet setup a profile, please add some info</p>
           <Link to='/create-profile' className='btn btn-primary my-1'>
             Create Profile
           </Link>
