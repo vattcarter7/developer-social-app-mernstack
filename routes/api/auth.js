0bb572de5dfedacae92b9c302f0a53a9ae29e30c
vattcarter7 <vattcarter@gmail.com>
2020-06-06 14:27:43 +0700

    implemented auth action and auth reducer

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -27,7 +27,7 @@ router.get('/', auth, async (req, res) => {
 router.post(
   '/',
   [
-    check('email', 'Please includes a valid email').isEmail(),
+    check('email', 'Please include a valid email').isEmail(),
     check('password', 'Password is required').exists()
   ],
   async (req, res) => {
