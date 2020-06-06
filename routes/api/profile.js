e065154d3d9237ed68603d9941b3681bbd585480
vattcarter7 <vattcarter@gmail.com>
2020-06-06 23:36:04 +0700

    added EditProfile page and fixed skills array bug

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -70,12 +70,15 @@ router.post(
     if (bio) profileFields.bio = bio;
     if (status) profileFields.status = status;
     if (githubusername) profileFields.githubusername = githubusername;
-    if (skills) {
-      profileFields.skills = skills.split(',').map((skill) => skill.trim());
-    }
+    // if (skills) {
+    //   profileFields.skills = skills.split(',').map((skill) => skill.trim());
+    // }
 
-    // Build social object
-    profileFields.social = {};
+    skills: Array.isArray(skills)
+      ? skills
+      : skills.split(',').map((skill) => ' ' + skill.trim()),
+      // Build social object
+      (profileFields.social = {});
     if (youtube) profileFields.social.youtube = youtube;
     if (twitter) profileFields.social.twitter = twitter;
     if (facebook) profileFields.social.facebook = facebook;
