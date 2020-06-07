f3dda64b9d13c9ccdb0f0f76b6ac8dd3ace4ae09
vattcarter7 <vattcarter@gmail.com>
2020-06-07 16:42:26 +0700

    implemented delete experience education and account

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,7 @@ const { check, validationResult } = require('express-validator');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 
 // @route     GET api/profile/me
 // @desc      Get current user profile
@@ -150,8 +151,8 @@ router.get('/user/:user_id', async (req, res) => {
 // @access    Private
 router.delete('/', auth, async (req, res) => {
   try {
-    //TODO - remove users posts
-    // Remove profile
+    // remove user posts
+    await Post.deleteMany({ user: req.user.id });
 
     // remove profile
     await Profile.findOneAndDelete({ user: req.user.id });
