20cad3527442728d5ba6d392b9baf29d26c68000
vattcarter7 <vattcarter@gmail.com>
2020-06-07 22:14:24 +0700

    add ProfileExperience

---
+++ client/src/components/profile/ProfileExperience.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ProfileExperience = (props) => {
+  return <div>h</div>;
+};
+
+ProfileExperience.propTypes = {};
+
+export default ProfileExperience;
