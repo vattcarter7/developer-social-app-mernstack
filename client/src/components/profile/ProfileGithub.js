0ed9350bb42c7bee920d114a6557d7dc18c14e42
vattcarter7 <vattcarter@gmail.com>
2020-06-08 14:10:47 +0700

    added github repos profile and prfile education

---
+++ client/src/components/profile/ProfileGithub.js
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getGithubRepos } from '../../actions/profile';
+import Spinner from '../layout/Spinner';
+
+const ProfileGithub = ({ username, getGithubRepos, repos }) => {
+  useEffect(() => {
+    getGithubRepos(username);
+  }, [getGithubRepos]);
+
+  return (
+    <div className='profile-github'>
+      <h2 className='text-primary my-1'>Github Repos</h2>
+      {repos === null ? (
+        <Spinner />
+      ) : (
+        repos.map((repo, index) => (
+          <div key={index} className='repo bg-white p-1 my-1'>
+            <h4>
+              <a href={repo.html_url} target='_blank' rel='noopener noreferrer'>
+                {repo.name}
+              </a>
+            </h4>
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
+
+ProfileGithub.propTypes = {
+  getGithubRepos: PropTypes.func.isRequired,
+  repos: PropTypes.array.isRequired,
+  username: PropTypes.string.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  repos: state.profile.repos
+});
+
+export default connect(mapStateToProps, { getGithubRepos })(ProfileGithub);
