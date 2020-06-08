0b8d89eb090282c196b13f6337351c2d272f6a19
vattcarter7 <vattcarter@gmail.com>
2020-06-08 14:29:34 +0700

    implemented github stars watchers forks and fixed bug for profile reducer

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -17,11 +17,29 @@ const ProfileGithub = ({ username, getGithubRepos, repos }) => {
       ) : (
         repos.map((repo, index) => (
           <div key={index} className='repo bg-white p-1 my-1'>
-            <h4>
-              <a href={repo.html_url} target='_blank' rel='noopener noreferrer'>
-                {repo.name}
-              </a>
-            </h4>
+            <div>
+              <h4>
+                <a
+                  href={repo.html_url}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  {repo.name}
+                </a>
+              </h4>
+              <p>{repo.description}</p>
+            </div>
+            <div>
+              <ul>
+                <li className='badge badge-primary'>
+                  Stars: {repo.stargazers_count}
+                </li>
+                <li className='badge badge-dark'>
+                  Watchers: {repo.watchers_count}
+                </li>
+                <li className='badge badge-light'>Forks: {repo.forks_count}</li>
+              </ul>
+            </div>
           </div>
         ))
       )}
