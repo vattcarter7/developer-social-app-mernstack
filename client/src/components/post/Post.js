75ae2b9752ce5098d563d328cdae96a9eab6294f
vattcarter7 <vattcarter@gmail.com>
2020-06-09 17:18:15 +0700

    implemented CommentItem displaying and removing comment

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,8 +4,9 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import PostItem from '../posts/PostItem';
-import { getPost } from '../../actions/post';
 import CommentForm from './CommentForm';
+import CommentItem from './CommentItem';
+import { getPost } from '../../actions/post';
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
   useEffect(() => {
@@ -21,6 +22,11 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
       </Link>
       <PostItem post={post} showActions={false} />
       <CommentForm postId={post._id} />
+      <div className='comment'>
+        {post.comments.map((comment) => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
     </Fragment>
   );
 };
